e97577ac6f341a8ded8887b49c3f625d0017cd89
oihdqij <oihdqij@A1290A>
2018-02-25 12:52:03 +0530

    Added CreateLead

---
+++ src/main/java/pages/CreateLead.java
@@ -0,0 +1,54 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+import wdMethods.ProjectMethods;
+
+public class CreateLead extends ProjectMethods{
+
+
+	public CreateLead() {
+
+		PageFactory.initElements(driver, this);
+
+	}
+
+
+	@FindBy(how=How.ID, using="createLeadForm_companyName")
+	WebElement comName; 
+	public CreateLead companyName() {
+		type(comName, "Google");
+		return this; 
+	}
+
+
+	@FindBy(how=How.ID,using="createLeadForm_firstName")
+	WebElement firName; 
+	public CreateLead firstName () {
+		type(firName, "Amina");
+		return this;
+	}
+
+
+	@FindBy(how=How.ID,using="createLeadForm_lastName")
+	WebElement lasName; 
+	public CreateLead lastName() {	
+		type(lasName, "Fathima");
+		return this; 
+
+	}
+	
+	@FindBy(how=How.XPATH,using="//input[@name='submitButton']")
+	WebElement subButton; 
+	public CreateLead clickSubmit() {
+		
+		click(subButton);
+		return this;
+	}
+	
+	
+
+}
